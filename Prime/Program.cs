73145eefe3f67799412ed1aed8420b79235660c1
glenbuck503 <glenbuck503@gmail.com>
2021-02-24 15:16:40 -0800

    fix indentation
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -8,19 +8,18 @@ namespace PrimeNumber
   {
     public static void Main()
     {
-      Console.WriteLine("Enter a number to see if its a Prime number or not:");
+      Console.WriteLine("Enter a number to see if it's a Prime number or not:");
       string userNumber = Console.ReadLine();
       int userInput = int.Parse(userNumber);
 
       Calculator number = new Calculator();
       if (number.NumberSifter(userInput) == true)
-    
       {
         Console.WriteLine("This is a Prime number!"); 
-      } else {
+      } else 
+      {
         Console.WriteLine("This is not a Prime number.");
       }
-      
     }
   }
 }
